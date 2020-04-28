65791cb880a531f1868c6aacf11d5ebb45296b26
Steven Bruno <sbruno636@gmail.com>
2020-04-27 19:54:51 -0500

    apply linting

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
+router.get('/', function (req, res, next) {
   res.json([
-    { id: 1, name: "steven" },
-    { id: 2, name: "john" },
+    { id: 1, name: 'steven' },
+    { id: 2, name: 'john' },
   ]);
 });
 
