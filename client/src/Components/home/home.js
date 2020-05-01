97fa6a47106c47efda5ee5976b2659bd2a2aaeb6
Steven Bruno <sbruno636@gmail.com>
2020-04-30 21:51:03 -0500

    update landing page styles

--- client/src/Components/home/home.js
+++ client/src/Components/home/home.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import homeStyles from './home.module.css';
 
 export default function Home() {
   return (
     <>
-      <h1>Wonderful Wishlist</h1>
-      <p>Create and share wishlists with friends and family.</p>
+      <div>
+        <h1>Wonderful Wishlist</h1>
+        <p>
+          Create and share wishlists with friends and family. Let them
+          collaborate and claim gifts, entirely hidden from your view.
+        </p>
+        <button className={homeStyles.landingButton}>Create wishlist</button>
+      </div>
     </>
   );
 }
