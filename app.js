b60c968165f0ee66fbd774b874da8fa716ce0d32
Steven Bruno <sbruno636@gmail.com>
2020-05-02 15:12:17 -0500

    implement login and register backend auth

--- app.js
+++ app.js
@@ -7,12 +7,19 @@ const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const logger = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 const testRouter = require('./routes/api/testdata');
+const usersRouter = require('./routes/api/users');
+
+// Passport config
+require('./config/passport')(passport);
+
+const app = express();
 
 //Set up mongoose connection
 const db_key = process.env.DB_USER_KEY;
@@ -21,11 +28,25 @@ mongoose.connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-const app = express();
+// Passport middleware
+app.use(passport.initialize());
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(bodyParser.json());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
 
 //enable cors
 app.use(cors());
@@ -38,6 +59,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/testdata', testRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema; // Create Schema
+const UserSchema = new Schema({
+  firstname: {
+    type: String,
+    required: false,
+  },
+  lastname: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = User = mongoose.model('users', UserSchema);
