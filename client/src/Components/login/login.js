3578733ba28042f10c43dcac624030b29e340bf6
Steven Bruno <sbruno636@gmail.com>
2020-05-02 15:11:39 -0500

    create signupcard component

--- client/src/Components/login/login.js
+++ client/src/Components/login/login.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import SignupCard from './signupCard';
 
 export default function Login() {
-  return <span>login</span>;
+  return (
+    <>
+      <span>Login or Signup</span>
+      <SignupCard />
+    </>
+  );
 }
---
+++ client/src/Components/login/signupCard.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+export default function SignupCard() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [password2, setPassword2] = useState('');
+
+  return (
+    <>
+      <h1>Sign up</h1>
+      <form>
+        <label for="email">Email</label>
+        <input
+          name="email"
+          placeholder="Email@domain.com"
+          type="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label for="password">Password</label>
+        <input
+          name="password"
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <label for="password2">Confirm Password</label>
+        <input
+          name="password2"
+          placeholder="Confirm password"
+          type="password"
+          value={password2}
+          onChange={(e) => setPassword2(e.target.value)}
+        />
+        <button type="button">Sign up</button>
+      </form>
+    </>
+  );
+}
