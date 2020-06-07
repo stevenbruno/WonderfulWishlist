5be5f7dff4e1cfcea65a292bffe653690e363e5b
Steven Bruno <sbruno636@gmail.com>
2020-06-07 17:47:02 -0500

    create route for testing passport-jwt middleware

--- routes/api/test.js
+++ routes/api/test.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET testdata listing. */
+/* GET test listing. */
 router.get('/', function (req, res, next) {
   res.json([
     { id: 1, name: 'steven' },
