045d4cb2b229e4095565f1f318a9f78f79195888
Steven Bruno <sbruno636@gmail.com>
2020-08-04 08:51:03 -0500

    remove password confirmation requirement

--- client/src/Components/login/signupCard.js
+++ client/src/Components/login/signupCard.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 export default function SignupCard() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [password2, setPassword2] = useState('');
 
   return (
     <>
@@ -24,14 +23,6 @@ export default function SignupCard() {
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-        />
-        <label htmlFor="password2">Confirm Password</label>
-        <input
-          name="password2"
-          placeholder="Confirm password"
-          type="password"
-          value={password2}
-          onChange={(e) => setPassword2(e.target.value)}
         />
         <button className="primarybtn" type="button">
           Sign up
